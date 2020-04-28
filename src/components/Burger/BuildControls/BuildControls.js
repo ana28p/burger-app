f1328669190368859247b383b79c25019497934e
ana28p <p.anamaria28@gmail.com>
2020-04-28 17:32:32 +0200

    Show total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,14 +5,15 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    { label: 'Salad', type: 'salad' },
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
     { label: 'Meat', type: 'meat' },
+    { label: 'Salad', type: 'salad' },
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p> Current price: <strong> {props.price.toFixed(2)} </strong> </p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
